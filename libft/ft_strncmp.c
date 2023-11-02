0f1fb28a6272cf9cd87d3a4b1fc45112e357349a
Rui Miguel Soares Martins <rusoares@c1r8s1.42porto.com>
2023-11-02 20:34:46 +0000

    Updated

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:46:56 by rusoares          #+#    #+#             */
-/*   Updated: 2023/11/02 18:56:03 by rusoares         ###   ########.fr       */
+/*   Updated: 2023/11/02 20:34:05 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t	i;
+
+	i = 0;
 	if (n == 0)
 	{
 		return (0);
 	}
-	while (*s1 != (const char)n)
+	while (s1[i] != '\0' && s1[i] == s2[i] && i < n - 1)
 	{
-		if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
-		s1++;
-		s2++;
+		i++;
 	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char )s2[i]);
 }
