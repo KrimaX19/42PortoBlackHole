f606724b883173566313de8573eab9d9a89861e3
Rui Miguel Soares Martins <rusoares@c1r8s1.42porto.com>
2023-11-02 18:14:04 +0000

    update

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -3,17 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:16:20 by rusoares          #+#    #+#             */
-/*   Updated: 2023/10/21 17:40:44 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/02 18:13:40 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-#include <stdio.h>
-#include <string.h>
-*/
 #include <unistd.h>
 
 void	ft_bzero(void *s, size_t n)
@@ -24,11 +20,3 @@ void	ft_bzero(void *s, size_t n)
 		--n;
 	}
 }
-/*
-int main (void)
-{
-  char str[] = "almost every programmer should know memset!";
-  ft_bzero (str,6 );
-  puts (str);
-  return 0;
-}*/
