d2775af2d9b06951a4cb6e6aa29bbd2d2c414753
Rui Miguel Soares Martins <rusoares@c1r5s1.42porto.com>
2023-11-03 21:12:13 +0000

    Updated1

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:47:04 by rusoares          #+#    #+#             */
-/*   Updated: 2023/11/03 21:04:16 by rusoares         ###   ########.fr       */
+/*   Updated: 2023/11/03 21:11:07 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,28 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-size_t	i;
-size_t	j;
-size_t	k;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 
-i = 0;
-k = 0;
-while (little[k] != '\0')
-{
-	k++;
-}
-if (!*little)
-{
-	return ((char *)big);
-}
-while (big[i] != '\0' && i < len)
-{
-	j = 0;
-	while (big[i+j] == little[j] &&little[j] != '\0' && (i+j) < len)
+	i = 0;
+	k = 0;
+	while (little[k] != '\0')
+		k++;
+	if (!*little)
+		return ((char *)big);
+	while (big[i] != '\0' && i < len)
 	{
-		j++;
+		j = 0;
+		while (big[i + j] == little[j] && little[j] != '\0' && (i + j) < len)
+		{
+			j++;
+		}
+		if (j == k)
+		{
+			return ((char *)(big + i));
+		}
+		i++;
 	}
-	if (j == k)
-	{
-		return ((char *)(big + i));
-	}
-	i++;
-}
-return (NULL);
+	return (NULL);
 }
