1f2d2d73844d900b3288c5b668ce78573364c801
Rui Miguel Soares Martins <rusoares@c1r5s1.42porto.com>
2023-11-03 22:43:52 +0000

    Update2

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:26:09 by rusoares          #+#    #+#             */
-/*   Updated: 2023/11/02 18:26:13 by rusoares         ###   ########.fr       */
+/*   Updated: 2023/11/03 22:43:25 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,32 @@
 int	ft_atoi(const char *nptr)
 {
 	long int	a;
+	int	i;
+	int s;
 
-	while (*nptr != '\0')
+	i = 0;
+	s = 1;
+	while ((nptr[i] == ' ') || (nptr[i] >= 9 && nptr[i] <= 13))
 	{
-		if (*nptr >= '0' && *nptr <= 9)
-		{
-			a = (long int )nptr;
-			a *= 10;
-			nptr++;
-		}
-		else
-		{
-			return (0);
-		}
+		i++;
 	}
-	return (a);
+	if (nptr[i] == '-')
+	{
+	s = -1;
+	i++;
+	}
+	if (nptr[i] >= '0' &&  nptr[i] <= '9')
+	{
+		a = (nptr[i] - '0');
+		i++;
+		while (nptr[i] >= '0' &&  nptr[i] <= '9')
+		{
+		a *= 10;
+		a += nptr[i] - '0';
+		i++;
+		}
+	return (a *(s));
+	}
+return (0);
 }
+
