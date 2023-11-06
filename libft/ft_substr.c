b3db55e48dfda4201d11998be7bb96385d980e51
Rui Miguel Soares Martins <rusoares@c1r15s15.42porto.com>
2023-11-06 20:07:33 +0000

    Update V1

---
+++ libft/ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 19:50:16 by rusoares          #+#    #+#             */
+/*   Updated: 2023/11/06 20:07:27 by rusoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Function name: ft_substr
+Prototype: char *ft_substr(char const *s, unsigned int start, size_t len);
+
+Turn in files -
+
+Parameters s: 
+The string from which to create the substring.
+start: The start index of the substring in the
+string ’s’.
+len: The maximum length of the substring.
+
+Return value: The substring. NULL if the allocation fails.
+
+External functs.: malloc
+Description: Allocates (with malloc(3)) and returns a substring from the string ’s’.
+The substring begins at index ’start’ and is of maximum size ’len’.
+*/
