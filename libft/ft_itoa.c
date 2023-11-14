5644a0b5c315149aced76ba874e828c7c89a9d4f
Rui Miguel Soares Martins <rusoares@c1r14s1.42porto.com>
2023-11-14 21:03:55 +0000

    Norminette OK

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,12 +6,16 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 21:04:10 by rusoares          #+#    #+#             */
-/*   Updated: 2023/11/14 20:16:05 by rusoares         ###   ########.fr       */
+/*   Updated: 2023/11/14 21:03:26 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <libft.h>
+
+int		contar(int n);
+int		negativo(int n);
+long	conver(long n);
 
 char	*ft_itoa(int n)
 {
@@ -20,29 +24,16 @@ char	*ft_itoa(int n)
 	long	nb;
 	char	*conv;
 
-	i = 0;
-	nb = n;
-	neg = 0;
-	if (n < 0)
-	{
-		neg = 1;
-		nb *= -1;
-		i++;
-	}
+	nb = conver(n);
+	neg = negativo(n);
+	i = contar(n);
 	if (n == 0)
-	{
 		i = 1;
-	}
-	while (n != 0)
-	{
-		n /= 10;
+	if (neg == 1)
 		i++;
-	}
 	conv = malloc (sizeof (char) * i + 1);
 	if (!conv)
-	{
 		return (NULL);
-	}
 	conv[i] = '\0';
 	while (i != 0)
 	{
@@ -50,8 +41,40 @@ char	*ft_itoa(int n)
 		nb /= 10;
 	}
 	if (i == 0 && neg == 1)
-	{
 		conv[i] = '-';
-	}
 	return (conv);
+}
+
+int	contar(int n)
+{
+	long	i;
+
+	i = 0;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+int	negativo(int n)
+{
+	if (n < 0)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+long	conver(long n)
+{
+	long	nb;
+
+	if (n < 0)
+	{
+		nb = n * -1;
+		return (nb);
+	}
+	return (n);
 }
